54afc85d711443b67fbb2c906f9b84dd8bab6106
shvn19 <shvn19@gmail.com>
2022-09-11 11:35:46 +0700

    Add Banner

---
+++ src/pages/Home/Banner/index.js
@@ -0,0 +1,12 @@
+export const Banner = () => {
+  return (
+    <div className="relative">
+      <img src="./banner1.jpg" />
+      <div className="absolute top-24 left-12 w-[300px] grid gap-4 italic">
+        <p className="text-slate-500 italic text-2xl">NEW INSPIRATION 2020</p>
+        <p className="text-color-primary text-4xl">20% OFF ON NEW SEASON</p>
+        <button className="bg-color-primary px-3 py-2 text-xl text-white mt-4">Browse collections</button>
+      </div>
+    </div>
+  )
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,11 @@
+import { Banner } from "./Banner"
+
 export const Home = () => {
   return (
-    <>
+    <div className="px-[100px]">
+      <Banner />
       <p>This is Home page</p>
-    </>
+    </div>
+      
   )
 }
